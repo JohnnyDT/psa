78f1e3154c4d6dabc0fbd07671167d2d77462f5f
JohnnyDT <janci.dt@gmail.com>
2021-03-07 13:03:27 +0100

    Zjednodusenie - uprava premennej

--- Cvicenie 02/cvicenie-02-uloha1.py
+++ Cvicenie 02/cvicenie-02-uloha1.py
@@ -12,16 +12,15 @@
 print("\nULOHA 1 \n")
 
 narast = 1.1                                            # narast na dalsi den 10% (vynasobenie 1.1)
-km_prvy_den = int(input("Zadaj km pre prvy den: "))     # pocet km v prvy en
+km_odbehnute = int(input("Zadaj km pre prvy den: "))     # pocet km v prvy en
 km_cielove = int(input("Zadaj cielovy pocet km: "))     # pocet cielovych km
 
-km_nasledujuci_den = km_prvy_den
 den = 1
 
-while km_nasledujuci_den < km_cielove:
+while km_odbehnute < km_cielove:
     den = den + 1
-    km_nasledujuci_den = km_nasledujuci_den * narast
+    km_odbehnute *= narast
 
-print("---> na " + str(den) + ".den prebehne " + str(round(km_nasledujuci_den, 2)) + " km")
+print("---> na " + str(den) + ".den prebehne " + str(round(km_odbehnute, 2)) + " km")
 
 # Floating Point Math
